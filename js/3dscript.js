4b77219f142abdd67ebb0ce1383b95f32fd3b545
JaeSeok <wotjr5124@gmail.com>
2023-12-27 10:01:27 +0900

    commit

--- js/3dscript.js
+++ js/3dscript.js
@@ -57,10 +57,10 @@ controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
 
 // Camera 위치 설정
-camera.position.z = 8;
+camera.position.z = 7;
 
 // AmbientLight 및 PointLight 추가
-const ambientLight = new THREE.AmbientLight(0x999999, 0.9); // 연한 회색 조명
+const ambientLight = new THREE.AmbientLight(0x999999); // 연한 회색 조명
 scene.add(ambientLight);
 
 const blueLight = new THREE.PointLight(0x0000ff, 4, 100);
@@ -68,7 +68,7 @@ blueLight.position.set(0, 10, 0);
 scene.add(blueLight);
 
 // Directional Light 수정
-const directionalLight = new THREE.DirectionalLight(0xff6699, 0.5); // 분홍색 조명
+const directionalLight = new THREE.DirectionalLight(0xff6699, 0.4); // 분홍색 조명
 directionalLight.position.set(5, 5, 5);
 directionalLight.castShadow = true; // 그림자 효과 활성화
 scene.add(directionalLight);
